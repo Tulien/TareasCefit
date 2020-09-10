3c143af769823a47cf12ff3e994bb7a9e7b0f922
Yulian <jarboleda030@misena.edu.co>
2020-09-10 17:07:40 -0500

    HTML BASE

---
+++ js/acciones.js
@@ -0,0 +1,16 @@
+var btn0 = document.getElementById('btnAgregar');
+var btn1 = document.getElementById("btnEditar");
+var btn2 = document.getElementById("btnEliminar");
+
+
+btn0.addEventListener("click", function(){
+    alert ("Aqui agregaras un nuevo archivo :D");
+});
+
+btn1.addEventListener("click", function(){
+    alert ("Aqui editaras el archivo seleccionado :D");
+});
+
+btn2.addEventListener("click", function(){
+    alert ("Aqui eliminaras el archivo seleccionado :D");
+});
