66af3395e605e1242cfa59fc54e7527458f6475e
Yulian <jarboleda030@misena.edu.co>
2020-09-17 13:25:16 -0500

    las filas se agregan con los datos correspondientes al boton de agregar

--- js/acciones.js
+++ js/acciones.js
@@ -2,11 +2,13 @@ var btn0 = document.getElementById("btnAgregar");
 var tabla = document.getElementById("tablaRegistros");
 
 btn0.addEventListener("click", function(){
-    var codi =  document.getElementById("txtCodigo");
-    var nom  =  document.getElementById("txtNombre");
-    var tel =  document.getElementById("txtTelefono");
-    var muni =  document.getElementById("txtMunicipio");
+    var codi =  document.getElementById("txtCodigo").value;
+    var nom  =  document.getElementById("txtNombre").value;
+    var tel =  document.getElementById("txtTelefono").value;
+    var muni =  document.getElementById("txtMunicipio").value;
 
-   tabla.insertRow(-1).innerHTML ="<td></td><td></td><td></td><td></td><td></td>" ;
+tabla.insertRow(-1).innerHTML ="<td>"+codi+"</td><td>"+nom+"</td><td>"+tel+"</td><td>"+muni+"</td><td></td>" ;
 });
+
+
 
